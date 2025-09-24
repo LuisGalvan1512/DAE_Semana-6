f2110cd04b74f80179321853a5351d82ba5506dc
Luis Enrique Galvan Morales <lusen121502@gmail.com>
2025-09-24 15:17:34 -0500

    Model and Serializers, quizzes

--- quiz-ai/quizzes/serializers.py
+++ quiz-ai/quizzes/serializers.py
@@ -1,29 +1,7 @@
 from rest_framework import serializers
-from .models import Quiz, Question, Choice
+from .models import Quiz
 
-class ChoiceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Choice
-        fields = ['id', 'text', 'is_correct']
-
-class QuestionDetailSerializer(serializers.ModelSerializer):
-    choices = ChoiceSerializer(many=True, read_only=True)
-    
-    class Meta:
-        model = Question
-        fields = ['id', 'quiz', 'text', 'choices']
-
-class QuizDetailSerializer(serializers.ModelSerializer):
-    questions = serializers.SerializerMethodField()
-    
+class QuizSerializer(serializers.ModelSerializer):
     class Meta:
         model = Quiz
-        fields = ['id', 'title', 'description', 'created_at', 'questions']
-    
-    def get_questions(self, obj):
-        questions = obj.questions.all()
-        return QuestionDetailSerializer(questions, many=True).data
-
-class AnswerSerializer(serializers.Serializer):
-    question_id = serializers.IntegerField()
-    choice_id = serializers.IntegerField()
+        fields = ['id', 'title', 'description', 'created_at']
