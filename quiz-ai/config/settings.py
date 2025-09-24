a7d2e02d5cfe605754cf1bf8e82b275ae036bd26
Luis Enrique Galvan Morales <lusen121502@gmail.com>
2025-09-24 14:54:46 -0500

    Coding

--- quiz-ai/config/settings.py
+++ quiz-ai/config/settings.py
@@ -37,7 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # 🆕
+    'quizzes',         # 🆕
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
